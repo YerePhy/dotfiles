01a5c251a1c49a39740645131cfe70d7de3c4d7f
Yere Poveda <jereasap@gmail.com>
2022-11-16 00:08:09 +0100

    Dotfiles are installed only if their associated symlinks don't exist.

--- install_dotfiles.sh
+++ install_dotfiles.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
-find . -type f -regextype sed -regex ".*\/\.[a-z\_\.]\+" | xargs -I {} readlink -f {} | xargs -I {} ln -s {} $HOME
+export DOTFILES=$(find . -type f -regextype sed -regex ".*\/\.[a-z\_\.]\+" -printf '%P\n') 
+
+for dfile in $DOTFILES
+do 
+    export SRC=$(readlink -f ./$dfile)
+    export DEST="$HOME"/$dfile 
+
+    if ! [ -L $DEST ] ; then
+        ln -s $SRC $DEST
+    fi
+done
